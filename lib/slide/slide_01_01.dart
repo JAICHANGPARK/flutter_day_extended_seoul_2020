3001bbbc17b707fa7f3bd1986aa2fb78f7b32dea
Dreamwalker <aristojeff@gmail.com>
2020-06-26 17:05:42 +0900

    Update slide_01_01.dart

--- lib/slide/slide_01_01.dart
+++ lib/slide/slide_01_01.dart
@@ -1,37 +1,91 @@
 import 'package:flutter/material.dart';
 
-
 class Slide0101 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Expanded(
-          child: Stack(
-            children: [
-              Positioned(
-                left: 0,
-                top: 0,
-                bottom: 16,
-                right: 16,
-                child: Placeholder(),
-              ),
-              Positioned(
-                left: 16,
-                top: 16,
-                bottom: 0,
-                right: 0,
-                child: Placeholder(),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 16,
+                  top: 16,
+                  bottom: 0,
+                  right: 0,
+                  child: Card(
+                    color: Colors.blue,
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 16,
+                  right: 16,
+                  child: Card(
+                    elevation: 12,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
         Expanded(
-          child: Placeholder(),
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 16,
+                  top: 16,
+                  bottom: 0,
+                  right: 0,
+                  child: Card(
+                    color: Colors.blue,
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 16,
+                  right: 16,
+                  child: Card(
+                    elevation: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
         Expanded(
-          child: Placeholder(),
-        )
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 16,
+                  top: 16,
+                  bottom: 0,
+                  right: 0,
+                  child: Card(
+                    color: Colors.blue,
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 16,
+                  right: 16,
+                  child: Card(
+                    elevation: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
