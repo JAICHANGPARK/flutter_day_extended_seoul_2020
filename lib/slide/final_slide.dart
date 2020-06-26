56bf24e30ca43b03fc9202bf500a3d717d5eb1c7
Dreamwalker <aristojeff@gmail.com>
2020-06-26 18:45:56 +0900

    Create final_slide.dart

---
+++ lib/slide/final_slide.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class FinalSlide extends StatefulWidget {
+  @override
+  _FinalSlideState createState() => _FinalSlideState();
+}
+
+class _FinalSlideState extends State<FinalSlide> {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/FL18-Slide-97-v01.gif', fit: BoxFit.cover,
+      height: MediaQuery.of(context).size.height,
+      repeat: ImageRepeat.repeat,
+      width: MediaQuery.of(context).size.width,);
+  }
+}
