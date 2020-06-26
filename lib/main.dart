5e741f047b237cced78d0c5f71014f74e2975ccf
Dreamwalker <aristojeff@gmail.com>
2020-06-26 13:59:58 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:f_desktop/slide/second_page.dart';
+import 'package:f_desktop/slide/title_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'slide/intro_slide.dart';
@@ -13,23 +14,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Day Extended Seoul',
-      theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Flutter Day Extended Seoul'),
+      theme: ThemeData.light(),
+      home: SplashSlider(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -39,14 +31,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -68,17 +54,12 @@ class _MyHomePageState extends State<MyHomePage> {
           if (event.logicalKey.keyLabel == "[") {
             pageIndex--;
             if (pageIndex <= 0) {
-
                 pageIndex = 0;
-
             }
           } else if (event.logicalKey.keyLabel == "]") {
             pageIndex++;
             if(pageIndex > 2) {
-
                 pageIndex = 2;
-
-
             }
 
           }
