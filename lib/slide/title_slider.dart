3ec3c2ef643820cd3c912746b23dd2fb5edc5b86
Dreamwalker <aristojeff@gmail.com>
2020-06-26 13:59:56 +0900

    Update title_slider.dart

--- lib/slide/title_slider.dart
+++ lib/slide/title_slider.dart
@@ -1,3 +1,30 @@
 
+import 'package:f_desktop/main.dart';
 import 'package:flutter/material.dart';
 
+class SplashSlider extends StatefulWidget {
+  @override
+  _SplashSliderState createState() => _SplashSliderState();
+}
+
+class _SplashSliderState extends State<SplashSlider> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => MyHomePage(title: "Flutter Day Extended Seoul")
+        ));
+      },
+      child: Image.asset('assets/FL18-Slide-93-v01.gif', fit: BoxFit.cover,
+        height: MediaQuery.of(context).size.height,
+        repeat: ImageRepeat.repeat,
+        width: MediaQuery.of(context).size.width,),
+    );
+  }
+}
