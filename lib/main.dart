eb2abcf6dea59284c3b555382727ba7f12f5dc74
Dreamwalker <aristojeff@gmail.com>
2020-06-26 16:22:46 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:f_desktop/slide/slide_page_00.dart';
 import 'package:f_desktop/slide/title_slider.dart';
+import 'package:f_desktop/utils/countdown_timer.dart';
 import 'package:flutter/material.dart';
 
 import 'slide/intro_slide.dart';
 import 'slide/slide_01.dart';
+import 'slide/slide_02.dart';
 import 'slide/slide_page_01_test.dart';
 
 void main() {
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Day Extended Seoul',
+      title: 'Flutter Day Extended Seoul 2020',
       theme: ThemeData.light(),
       home: SplashSlider(),
     );
@@ -32,15 +36,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final FocusNode _focusNode = FocusNode();
   int _pageIndex = 0;
+  Timer _timer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 64),
+            child: CountdownFormatted(
+              builder: (BuildContext ctx, String remaining) {
+                return Center(
+                    child: Text(
+                  remaining,
+                  style: TextStyle(fontSize: 24),
+                )); // 01:00:00
+              },
+              duration: Duration(minutes: 20),
+            ),
+          )
+        ],
       ),
       body: RawKeyboardListener(
         focusNode: _focusNode,
@@ -48,10 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
           print(event);
           print(_pageIndex);
           if (event.logicalKey.keyLabel == "[") {
-
-          } else if (event.logicalKey.keyLabel == "]") {
-
-          }
+          } else if (event.logicalKey.keyLabel == "]") {}
         },
         child: Stack(
           children: [
@@ -59,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
               index: _pageIndex,
               children: [
                 Slide01(),
-                Slide00(),
+                Slide02(),
                 Slide00(),
                 Slide00(),
               ],
@@ -69,31 +92,30 @@ class _MyHomePageState extends State<MyHomePage> {
               bottom: 16,
               child: Row(
                 children: [
-
-                  _pageIndex != 0?  FloatingActionButton(
-                    onPressed: () {
-                      _pageIndex--;
-                      if (_pageIndex <= 0) {
-                        _pageIndex = 0;
-                      }
-                      setState(() {
-
-                      });
-                    },
-                    tooltip: 'pREV',
-                    heroTag: "Prev Page",
-                    child: Icon(Icons.arrow_back),
-                  ) : Container(),
-                  SizedBox(width: 16,),
+                  _pageIndex != 0
+                      ? FloatingActionButton(
+                          onPressed: () {
+                            _pageIndex--;
+                            if (_pageIndex <= 0) {
+                              _pageIndex = 0;
+                            }
+                            setState(() {});
+                          },
+                          tooltip: 'pREV',
+                          heroTag: "Prev Page",
+                          child: Icon(Icons.arrow_back),
+                        )
+                      : Container(),
+                  SizedBox(
+                    width: 16,
+                  ),
                   FloatingActionButton(
                     onPressed: () {
                       _pageIndex++;
                       if (_pageIndex > 2) {
                         _pageIndex = 2;
                       }
-                      setState(() {
-
-                      });
+                      setState(() {});
                     },
                     heroTag: "next",
                     tooltip: 'Next Page',
@@ -103,10 +125,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             )
           ],
-
         ),
       ),
-
     );
   }
 }
