00b5f82e1ca62c7ed499f36c905eed05747e5265
Dreamwalker <aristojeff@gmail.com>
2020-06-26 15:13:41 +0900

    Create slide_page_00.dart

---
+++ lib/slide/slide_page_00.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Slide00 extends StatefulWidget {
+  @override
+  _SecondSlidePageState createState() => _SecondSlidePageState();
+}
+
+class _SecondSlidePageState extends State<Slide00> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Second Slider"),
+    );
+  }
+}
