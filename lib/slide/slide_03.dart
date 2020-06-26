6aa408a8829a14a443b08d95343f8c67730d3647
Dreamwalker <aristojeff@gmail.com>
2020-06-26 20:10:11 +0900

    Update slide_03.dart

--- lib/slide/slide_03.dart
+++ lib/slide/slide_03.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class Slide03 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(36.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("🛡 Type Safe", style: GoogleFonts.montserrat(
+            fontWeight: FontWeight.bold,
+            fontSize: 140
+          ),),
+
+          Text("")
+
+        ],
+      ),
+    );
   }
 }
