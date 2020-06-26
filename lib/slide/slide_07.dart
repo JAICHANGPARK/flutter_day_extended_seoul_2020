896231d04ac0492de7e4204619768cd5c9dd6445
Dreamwalker <aristojeff@gmail.com>
2020-06-27 00:53:25 +0900

    Create slide_07.dart

---
+++ lib/slide/slide_07.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class Slide07 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.network("https://i1.wp.com/walkingtree.tech/wp-content/uploads/2020/04/flutter-1.png");
+  }
+}
