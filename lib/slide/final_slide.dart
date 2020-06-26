bd79f6ab328fac718ab40e36a6e30d86e66b5622
Dreamwalker <aristojeff@gmail.com>
2020-06-26 20:10:04 +0900

    Update final_slide.dart

--- lib/slide/final_slide.dart
+++ lib/slide/final_slide.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FinalSlide extends StatefulWidget {
   @override
   _FinalSlideState createState() => _FinalSlideState();
@@ -9,9 +8,20 @@ class FinalSlide extends StatefulWidget {
 class _FinalSlideState extends State<FinalSlide> {
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/FL18_Slide_97_v01.gif', fit: BoxFit.cover,
-      height: MediaQuery.of(context).size.height,
-      repeat: ImageRepeat.repeat,
-      width: MediaQuery.of(context).size.width,);
+    return Column(
+      children: [
+        Center(
+          child: Image.asset(
+            'assets/FL18_Slide_97_v01.gif',
+            height: MediaQuery.of(context).size.height / 1.2,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Text(
+          "Slide Link : https://github.com/JAICHANGPARK/flutter_day_extended_seoul_2020",
+          style: TextStyle(fontSize: 21),
+        )
+      ],
+    );
   }
 }
