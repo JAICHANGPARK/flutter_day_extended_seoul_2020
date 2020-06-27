4fd777389f461c64f1e67aaf9829ee314ffbb701
Dreamwalker <aristojeff@gmail.com>
2020-06-27 10:54:10 +0900

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:f_desktop/main.dart';
+import 'package:flutter_day_seoul/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
